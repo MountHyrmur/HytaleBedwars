a63c056a88298dfc851dfd57f6fb5c85e16d07bb
SzczurekYT <szczurek@szczurek.yt>
2026-01-23 14:34:10 +0100

    feat: Empty map world creation

--- src/main/java/yt/szczurek/hyrmur/bedwars/command/BedwarsMapActionCommand.java
+++ src/main/java/yt/szczurek/hyrmur/bedwars/command/BedwarsMapActionCommand.java
@@ -5,7 +5,7 @@ import com.hypixel.hytale.server.core.command.system.arguments.types.ArgTypes;
 import com.hypixel.hytale.server.core.command.system.basecommands.AbstractPlayerCommand;
 
 public abstract class BedwarsMapActionCommand extends AbstractPlayerCommand {
-    private final RequiredArg<String> mapNameArg = this.withRequiredArg("mapName", "server.commands.bedwars.edit.arg.name", ArgTypes.STRING);
+    protected final RequiredArg<String> mapNameArg = this.withRequiredArg("mapName", "server.commands.bedwars.edit.arg.name", ArgTypes.STRING);
 
     public BedwarsMapActionCommand(String literal) {
         super(literal, "server.commands.bedwars.map." + literal + ".desc", false);
