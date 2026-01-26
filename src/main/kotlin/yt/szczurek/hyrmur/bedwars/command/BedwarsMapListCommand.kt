bf228d4404d7e99797c82d3200b86f2546c6e450
SzczurekYT <szczurek@szczurek.yt>
2026-01-26 12:17:43 +0100

    feat: Working list command

--- src/main/kotlin/yt/szczurek/hyrmur/bedwars/command/BedwarsMapListCommand.kt
+++ src/main/kotlin/yt/szczurek/hyrmur/bedwars/command/BedwarsMapListCommand.kt
@@ -1,17 +1,23 @@
 package yt.szczurek.hyrmur.bedwars.command
 
+import com.hypixel.hytale.server.core.Message
 import com.hypixel.hytale.server.core.command.system.CommandContext
 import com.hypixel.hytale.server.core.command.system.basecommands.AbstractAsyncCommand
+import com.hypixel.hytale.server.core.util.message.MessageFormat
+import yt.szczurek.hyrmur.bedwars.asset.BedwarsMap
 import java.util.concurrent.CompletableFuture
-import javax.annotation.Nonnull
 
 class BedwarsMapListCommand : AbstractAsyncCommand("list", "server.commands.bedwars.map.list.desc") {
-    @Nonnull
-    public override fun executeAsync(context: CommandContext): CompletableFuture<Void?> {
-//        List<String> instanceAssets = InstancesPlugin.get().getInstanceAssets();
-//        context.sendMessage(
-//                MessageFormat.list(Message.translation("server.commands.instances.edit.list.header"), instanceAssets.stream().map(Message::raw).toList())
-//        );
-        return CompletableFuture.completedFuture<Void?>(null)
+
+    override fun executeAsync(ctx: CommandContext): CompletableFuture<Void> {
+        val mapAssets = BedwarsMap.assetMap.assetMap.keys
+        ctx.sendMessage(
+            MessageFormat.list(HEADER, mapAssets.stream().map(Message::raw).toList())
+        )
+        return CompletableFuture.completedFuture(null)
+    }
+
+    companion object {
+        val HEADER = Message.translation("server.commands.bedwars.map.list.header")
     }
 }
