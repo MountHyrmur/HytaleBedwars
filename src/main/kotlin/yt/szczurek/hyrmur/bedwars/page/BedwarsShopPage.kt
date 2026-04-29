c88ad566deafd7713ba645c6f4bc1978d5a66482
SzczurekYT <szczurek@szczurek.yt>
2026-04-29 23:01:27 +0200

    feat: Improved user feedback in shop ui

--- src/main/kotlin/yt/szczurek/hyrmur/bedwars/page/BedwarsShopPage.kt
+++ src/main/kotlin/yt/szczurek/hyrmur/bedwars/page/BedwarsShopPage.kt
@@ -7,20 +7,25 @@ import com.hypixel.hytale.component.Ref
 import com.hypixel.hytale.component.Store
 import com.hypixel.hytale.protocol.packets.interface_.CustomPageLifetime
 import com.hypixel.hytale.protocol.packets.interface_.CustomUIEventBindingType
+import com.hypixel.hytale.protocol.packets.interface_.NotificationStyle
+import com.hypixel.hytale.server.core.Message
+import com.hypixel.hytale.server.core.asset.type.item.config.Item
 import com.hypixel.hytale.server.core.entity.ItemUtils
+import com.hypixel.hytale.server.core.entity.entities.Player
 import com.hypixel.hytale.server.core.entity.entities.player.pages.InteractiveCustomUIPage
 import com.hypixel.hytale.server.core.inventory.InventoryComponent
 import com.hypixel.hytale.server.core.inventory.container.CombinedItemContainer
-import com.hypixel.hytale.server.core.inventory.transaction.ItemStackTransaction
 import com.hypixel.hytale.server.core.ui.builder.EventData
 import com.hypixel.hytale.server.core.ui.builder.UICommandBuilder
 import com.hypixel.hytale.server.core.ui.builder.UIEventBuilder
 import com.hypixel.hytale.server.core.universe.PlayerRef
 import com.hypixel.hytale.server.core.universe.world.storage.EntityStore
+import com.hypixel.hytale.server.core.util.NotificationUtil
 import yt.szczurek.hyrmur.bedwars.asset.BedwarsShop
 import yt.szczurek.hyrmur.bedwars.asset.BedwarsTeam
 import yt.szczurek.hyrmur.bedwars.asset.data.ShopTrade
 
+private val MESSAGE_MISSING_ITEM = Message.translation("server.customUI.bedwarsShop.error.missingItem")
 
 class BedwarsShopPage(
     playerRef: PlayerRef,
@@ -68,17 +73,15 @@ class BedwarsShopPage(
             )
         }
 
-        updatePage(ref, store, uiCommandBuilder, uiEventBuilder)
+        updatePage(uiCommandBuilder, uiEventBuilder)
+        updateBuyHint(ref, store, uiCommandBuilder)
     }
 
     private fun updatePage(
-        ref: Ref<EntityStore>,
-        store: Store<EntityStore>,
         uiCommandBuilder: UICommandBuilder,
         uiEventBuilder: UIEventBuilder
     ) {
         val page = shop.pages[selectedPageIndex]
-        val inventory = getInventory(ref, store)
 
         uiCommandBuilder.set("#TitleLabel.Text", page.title)
 
@@ -93,11 +96,6 @@ class BedwarsShopPage(
             // Price
             uiCommandBuilder.set("$selector #PriceSlot.ItemId", trade.input!!.itemId)
             uiCommandBuilder.set("$selector #PriceCount.Text", "${trade.input!!.quantity}")
-
-            val canBuy = canBuyTrade(trade, inventory)
-            if (canBuy) {
-                uiCommandBuilder.set("$selector #PriceSlotBorder.Background", "#37b57a")
-            }
 
             // Product
             uiCommandBuilder.set("$selector #ProductSlot.ItemId", trade.output!!.itemId)
@@ -114,27 +112,69 @@ class BedwarsShopPage(
         }
     }
 
+    private fun updateBuyHint(
+        ref: Ref<EntityStore>,
+        store: Store<EntityStore>,
+        uiCommandBuilder: UICommandBuilder,
+    ) {
+        val page = shop.pages[selectedPageIndex]
+        val inventory = getInventory(ref, store)
+
+        for ((i, trade) in page.trades.withIndex()) {
+            val canBuy = canBuyTrade(trade, inventory)
+            uiCommandBuilder.set("#ItemGrid[$i] #PriceSlotBorder.Background", if (canBuy) "#37b57a" else "#1a2530")
+        }
+    }
+
     fun getInventory(ref: Ref<EntityStore>, store: Store<EntityStore>): CombinedItemContainer {
         return InventoryComponent.getCombined(store, ref, InventoryComponent.Hotbar.getComponentType(), InventoryComponent.Storage.getComponentType())
     }
 
     fun canBuyTrade(trade: ShopTrade, inventory: CombinedItemContainer): Boolean {
-        return inventory.canRemoveItemStack(trade.toPriceItemStack()) && inventory.canAddItemStack(trade.toProductItemStack())
+        return inventory.canRemoveItemStack(trade.toPriceItemStack())
     }
 
+    fun tryExecuteTrade(ref: Ref<EntityStore>, store: Store<EntityStore>, tradeIndex: Int): Boolean {
         val trade = shop.pages[selectedPageIndex].trades[tradeIndex]
+
+        val player = store.getComponent(ref, Player.getComponentType())!!
 
         val inventory = getInventory(ref, store)
         val canBuy = canBuyTrade(trade, inventory)
 
         if (!canBuy) {
-            return
+            val priceTranslationKey = Item.getAssetMap().getAsset(trade.input!!.itemId)?.translationKey ?: return false
+            val productTranslationKey = Item.getAssetMap().getAsset(trade.output!!.itemId)?.translationKey ?: return false
+            NotificationUtil.sendNotification(
+                playerRef.packetHandler,
+                MESSAGE_MISSING_ITEM
+                    .param("missing", Message.translation(priceTranslationKey))
+                    .param("product", Message.translation(productTranslationKey)),
+                NotificationStyle.Danger
+            )
+            return false
         }
 
-        val transaction = inventory.removeItemStack(trade.toPriceItemStack())
-        if (transaction.succeeded()) {
-            inventory.addItemStack(trade.toProductItemStack())
+        val removeTransaction = inventory.removeItemStack(trade.toPriceItemStack())
+        if (!removeTransaction.succeeded()) {
+            throw IllegalStateException("Remove transaction failed while previous check suggested it shouldn't")
         }
+
+        val productItemStack = trade.toProductItemStack()
+        val addTransaction = inventory.addItemStack(productItemStack)
+
+        // Things that didn't fit
+        val remaining = addTransaction.remainder?.takeUnless { rem -> rem.isEmpty }?.quantity ?: 0
+        if (remaining == 0) {
+            player.notifyPickupItem(ref, productItemStack, null, store)
+        } else {
+            val added = productItemStack.getQuantity() - remaining
+            if (added > 0) {
+                player.notifyPickupItem(ref, productItemStack.withQuantity(added)!!, null, store)
+            }
+            ItemUtils.dropItem(ref, addTransaction.remainder!!, store)
+        }
+        return true
     }
 
     override fun handleDataEvent(
@@ -143,15 +183,24 @@ class BedwarsShopPage(
         data: BindingData
     ) {
         if (data.tradeIndex != null) {
-            tryExecuteTrade(ref, store, data.tradeIndex!!)
-            sendUpdate()
+            val success = tryExecuteTrade(ref, store, data.tradeIndex!!)
+            if (success) {
+                val uiBuilder = UICommandBuilder()
+                updateBuyHint(ref, store, uiBuilder)
+                sendUpdate(uiBuilder)
+            } else {
+                sendUpdate()
+            }
+            return
         }
         if (data.pageIndex != null) {
             selectedPageIndex = data.pageIndex!!
             val uiBuilder = UICommandBuilder()
             val eventBuilder = UIEventBuilder()
-            updatePage(ref, store, uiBuilder, eventBuilder)
+            updatePage(uiBuilder, eventBuilder)
+            updateBuyHint(ref, store, uiBuilder)
             sendUpdate(uiBuilder, eventBuilder, false)
+            return
         }
     }
 }
